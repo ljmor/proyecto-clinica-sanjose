ed20247357a507fd822576184be5d9ede86c2ffb
Luis Mora <ljmora004@outlook.com>
2025-01-24 19:01:10 -0500

    Semana 06

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
+import { getEnvVariables } from "./getEnvVariables";
+// Acceder a la variable de entorno que tiene el URL base para conectarnos al backend
+// Para acceder se necesita hacer uso del archivo getEnvVariables
+const { VITE_API_URL } = getEnvVariables();
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/',
+    // baseURL: 'http://localhost:3000/api/',
+    baseURL: VITE_API_URL,
     maxContentLength: Infinity, // Deshabilita el límite para el contenido
     maxBodyLength: Infinity,
 });
